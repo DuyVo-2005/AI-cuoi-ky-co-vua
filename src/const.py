cb11b9f7be2fd056fe664a86075c152154242de8
SangPhan-wpgg <phandinhsang2005@gmail.com>
2025-03-23 16:25:15 +0700

    Create const.py

---
+++ src/const.py
@@ -0,0 +1,22 @@
+# Kích thước màn hình pygame
+WIDTH = 600
+HEIGHT = 600
+
+# 
+ROWS = 8
+COLS = 8
+SQSIZE = WIDTH // COLS
+
+# 
+FIRST_PLAYER = "white"
+PLAYER_COLOR = "white"
+BOT_COLOR = "black"
+
+# 
+DELAY = 200
+
+# 
+START_FEN = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q2/PPPBBPpP/R3K2R w - - 0 1"
+# START_FEN = "8/k7/3p4/p2P1p2/P2P1P2/8/8/K7 w - - 0 1"
+# START_FEN = "r2k1r2/3q1b1p/1bp1p2n/8/2Nn1P2/1P2N1R1/P1BK3B/5Q2 w - - 1 1"
+# START_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
