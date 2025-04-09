fcee9ca23c7856bdba2405e25900a63fe59e7492
Võ Lê Khánh Duy <162585498+DuyVo-2005@users.noreply.github.com>
2025-04-10 06:22:38 +0700

    Create Generate_state_in_knight_tour.py

---
+++ src/Generate_state_in_knight_tour.py
@@ -0,0 +1,14 @@
+def GenerateKnightMoves(x, y):
+    moves = []# Danh sách chứa các nước đi hợp lệ
+    directions = [
+        (2, 1), (2, -1), (-2, 1), (-2, -1),
+        (1, 2), (1, -2), (-1, 2), (-1, -2)
+    ]
+    
+    for (dx, dy) in directions:
+        new_x = x + dx
+        new_y = y + dy
+        If 0 <= new_x <= 7 and 0 <= new_y <= 7):
+            moves.append((new_x, new_y))
+    
+    return moves
